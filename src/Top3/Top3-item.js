f7daa08decec89bf3ededa57ea2b8f9ad8791325
DanilZam <danil29041@gmail.com>
2023-06-11 22:41:02 +0300

    I wanna die

--- src/Top3/Top3-item.js
+++ src/Top3/Top3-item.js
@@ -19,19 +19,19 @@ export function Top3Item (props) {
         // window.location.href = props.item.profileLink
     }
 
+    console.log(props.img)
+
     return(
         <div key={props.item._id} className={props.styles.item}>
             <div className={props.styles.topSection}>
-                <img className={props.styles.ProfilePhoto} src={props.item.imageLink} alt="" />
+                <img className={props.styles.ProfilePhoto} src={props.img} alt="вв" />
                 <div className={props.styles.FIO}>
                     <p className={props.styles.F}>{parts[0]}</p>  
                     <p className={props.styles.IO}>{parts[1] + ' ' + parts[2]}</p>  
-                    {/* <p>Количество цитирований: {props.item.cited}</p> */}
                 </div>
-                {/* <button onClick={handleClick}>Узнать больше на Schoolar</button> */}
             </div>
             <p>Количество цитирований: {props.item.cited}</p>
-            {/* <button onClick={handleClick}>Узнать больше</button> */}
         </div>
     )
 }
+// src={require("./backgroundForTop/bronzebstu.jpg")}
--- src/Top3/Top3Profiles.js
+++ src/Top3/Top3Profiles.js
@@ -3,6 +3,10 @@ import styles from '../App.module.css'
 import { Top3Item } from './Top3-item'
 import { useEffect } from 'react'
 
+// import a from 'D:\Учёба\Веб\v2\client\backgroundForTop\silverbstu.jpg'
+
+
+
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchTop3 } from '../redux/slices/top3'
 
@@ -27,12 +31,20 @@ export function Top3Profiles() {
 
     return(
         <div className={styles.DivForTop3}>
-            <h3 className={styles.Top3Header}>ТОП 3 СОТРУДНИКА ПО ЦИТИРОВАНИЮ</h3>
-            <div className={styles.blockIt}>
-            {(isTop3Loading ? sample : top3.items).map(item => <Top3Item styles={styles} key={item.id} item={item} />)} 
-            {/*  {props.top3it.length ? (props.top3it.map(top => <Top3Item styles={props.styles} key={top.id} top={top} /> ) ) 
-            : <p>Отсутствуют данные по цитированию</p> }  */}       
+            {/* <h3 className={styles.Top3Header}>ТОП 3 СОТРУДНИКА ПО ЦИТИРОВАНИЮ</h3> */}
+            {/* <div className={styles.blockIt}>
+            {(isTop3Loading ? sample : top3.items).map(item => <Top3Item styles={styles} key={item.id} item={item} />)}     
+            </div> */}
+
+            {/* {!isTop3Loading&&(<div className={styles.NewTop3}>
+                <div className={styles.ForColumns}>
+                    <div className={styles.Columns}><img src={top3.items[1].imageLink} alt="dd" /><div className={styles.T2}>{isTop3Loading ? <Top3Item styles={styles} item={sample[0]}/> : <Top3Item styles={styles} item={top3.items[1]} img={require("./backgroundForTop/silverbstu1.png")}/>}</div></div>
+                    <div className={styles.Columns}><img src={top3.items[0].imageLink} alt="dd" /><div className={styles.T1}>{isTop3Loading ? <Top3Item styles={styles} item={sample[0]}/> : <Top3Item styles={styles} item={top3.items[0]} img={require("./backgroundForTop/goldbstu1.png")}/>}</div></div>
+                    <div className={styles.Columns}><img src={top3.items[2].imageLink} alt="dd" /><div className={styles.T3}>{isTop3Loading ? <Top3Item styles={styles} item={sample[0]}/> : <Top3Item styles={styles} item={top3.items[2]} img={require("./backgroundForTop/bronzebstu1.png")}/>}</div></div>
+                </div>
+                <div className={styles.bottomLine}></div>
+            </div>)}  */}
+
         </div>
-      </div>
     )
 }
